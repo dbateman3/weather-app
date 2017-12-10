640c78c0d1c024a787eb87d38b27233c13ca5ed2
Dan Bateman <dbateman0319@starkstate.net>
2017-12-10 00:47:54 -0500

    Added weather api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 const geocode = require("./geocode/geocode.js");
+const request = require("request");
 
 const argv = yargs
 	.options({
@@ -22,4 +23,17 @@ geocode.geocodeAddress(argv.address, function(errorMessage, results) {
 	} else {
 		console.log(JSON.stringify(results, undefined, 2));
 	}
+});
+
+// https://api.darksky.net/forecast/2df076a676988d220ee69dd45e020547/37.8267,-122.4233
+
+request({
+	url: 'https://api.darksky.net/forecast/2df076a676988d220ee69dd45e020547/37.8267,-122.4233',
+	json: true,
+	}, function(error, response, body) {
+		if (!error && response.statusCode === 200) {
+			console.log(body.currently.temperature);
+		} else {
+			console.log('Unable to get weather');
+		}
 });
