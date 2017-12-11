963dfc0ef0efed2a6b43dda6d2f81d7f47828fe3
Dan Bateman <dbateman0319@starkstate.net>
2017-12-10 22:21:38 -0500

    App now returns temp for entered address

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require("yargs");
 const geocode = require("./geocode/geocode.js");
-const request = require("request");
+const weather = require("./weather/weather.js");
+
 
 const argv = yargs
 	.options({
@@ -16,24 +17,19 @@ const argv = yargs
 	.argv;
 
 
-geocode.geocodeAddress(argv.address, function(errorMessage, results) {
+geocode.geocodeAddress(argv.address, function(errorMessage, geoResults) {
 	if (errorMessage) {
 		console.log(errorMessage);
 
 	} else {
-		console.log(JSON.stringify(results, undefined, 2));
+		console.log(`Address: ${geoResults.address}`);
+		weather.getWeather(geoResults.latitude, geoResults.longitude, function(errorMessage, weatherResults) {
+		if (errorMessage) {
+			console.log(errorMessage);
+		} else {
+			console.log(`It's currently ${weatherResults.temperature} degrees.`);
+		}
+	});
 	}
 });
 
-// https://api.darksky.net/forecast/2df076a676988d220ee69dd45e020547/37.8267,-122.4233
-
-request({
-	url: 'https://api.darksky.net/forecast/2df076a676988d220ee69dd45e020547/37.8267,-122.4233',
-	json: true,
-	}, function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			console.log(body.currently.temperature);
-		} else {
-			console.log('Unable to get weather');
-		}
-});
---
+++ weather/weather.js
@@ -0,0 +1,21 @@
+const request = require("request");
+
+
+let getWeather = function(lat, lng, callback) {
+	request({
+		url: `https://api.darksky.net/forecast/2df076a676988d220ee69dd45e020547/${lat},${lng}`,
+		json: true,
+		}, function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				callback(undefined, {
+					temperature: body.currently.temperature,
+				});
+			} else {
+				callback('Unable to get weather');
+			}
+	});
+};
+
+module.exports = {
+	getWeather: getWeather
+}
